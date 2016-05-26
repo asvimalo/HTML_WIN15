5404ee9c87db081a1c1ab55c6ecc0d881b24df63
asvimalo <asvimalo@gmail.com>
2016-05-26 18:25:44 +0200

    Presentation site lab3 finished

---
+++ Lesson/L26_05_16_bootstrap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L26_05_16_bootstrap.MvcApplication" Language="C#" %>
---
+++ Lesson/L26_05_16_bootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(L26_05_16_bootstrap.Startup))]
+namespace L26_05_16_bootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
