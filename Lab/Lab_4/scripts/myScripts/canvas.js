824a109e6b56f4c6b1015150fcfa163a59992dfa
asvimalo <asvimalo@gmail.com>
2016-06-08 14:51:09 +0200

    video done

---
+++ Lab/Lab_4/scripts/myScripts/canvas.js
@@ -0,0 +1,16 @@
+﻿
+        window.onload = function() {
+            var canvas = document.getElementById("myCanvas");
+            var ctx = canvas.getContext("2d");
+            
+            ctx.fillStyle = "#59cfff";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = "#c8ba8d";
+            ctx.fillRect (0,300, canvas.width, canvas.height);
+            
+        };
+        
+        
+        
+        
